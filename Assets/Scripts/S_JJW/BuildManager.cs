9d5cd0cae9711d5d07e5e23653859eef8c0ebe36
JooSK314 <92357621+JooSK314@users.noreply.github.com>
2021-12-17 17:53:29 +0900

    12_17

--- Assets/Scripts/S_JJW/BuildManager.cs
+++ Assets/Scripts/S_JJW/BuildManager.cs
@@ -3,91 +3,109 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 [System.Serializable]
 public class Craft  // 생성할 건물들을 분류할 클래스
 {
+        
+
     public string craftname; //건물 이름
     public GameObject previewCraft; //미리보기 프리펩
     public GameObject BuildCraft; // 실제 지어질 프리펩
 
 }
-
-public class BuildManager : MonoBehaviour
+namespace RTS.Player
 {
-
-
-
-    [SerializeField] private Craft[] craft = null;  //직렬화를 통해 인스펙터창에서 관리하기 위한 변수
-
-    private GameObject PreviewPrefab = null;    //Craft를 담을 변수와 미리보기에 사용할 변수 선언
-    private GameObject InsPrefab = null;    //생성할 건물
-
-    private bool isActivatePreview = false; //Preview가 만들어졌는지 확인할 bool값 변수
-
-    //
-    private RaycastHit hit;
-    private Vector3 _location;
-
-    private Vector3 buildPos;
-
-    private void Awake()
+    public class BuildManager : MonoBehaviour
     {
 
+        public static BuildManager instance = null;
 
-    }
-    private void Update()
-    {
+        [SerializeField] private Craft[] craft = null;  //직렬화를 통해 인스펙터창에서 관리하기 위한 변수
 
-        if (!isActivatePreview)
+        private GameObject PreviewPrefab = null;    //Craft를 담을 변수와 미리보기에 사용할 변수 선언
+        private GameObject InsPrefab = null;    //생성할 건물
+
+        public bool isActivatePreview = false; //Preview가 만들어졌는지 확인할 bool값 변수
+
+        //
+        private RaycastHit hit;
+        private Vector3 _location;
+
+        private Vector3 buildPos;
+
+
+        private void Awake()
         {
-            if (Input.GetKeyDown(KeyCode.R)) SlotClick(0);
-        }
-
-        if (isActivatePreview)
-        {
-            int layerMask = (1 << LayerMask.NameToLayer("ground")) + (1 << LayerMask.NameToLayer("Sea"));   //충돌검사를 할 레이어
-
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, layerMask))
+            if (instance == null)
             {
-                Debug.Log(hit.point);
-
-
-                _location = hit.point;
-                _location.y += 0.1f;
-
-                buildPos = new Vector3((int)_location.x, hit.point.y, (int)_location.z);
-                PreviewPrefab.transform.position = buildPos;
-
+                instance = this;
             }
-        }
 
-        if (PreviewPrefab != null && PreviewPrefab.GetComponentInChildren<Preview>().GetBuildable())
+        }
+        private void Update()
         {
-            if (Input.GetMouseButtonDown(0))
+
+            if (!isActivatePreview)
             {
-                Instantiate(InsPrefab, buildPos, Quaternion.identity);
+                if (Input.GetKeyDown(KeyCode.R)) SlotClick(0);
+                Debug.Log("클릭");
+            }
+
+            if (isActivatePreview)
+            {
+                int layerMask = (1 << LayerMask.NameToLayer("ground")) + (1 << LayerMask.NameToLayer("Sea"));   //충돌검사를 할 레이어
+
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, layerMask))
+                {
+                    //   Debug.Log(hit.point);
+
+                    Debug.Log("위치이동");
+                    _location = hit.point;
+                    _location.y += 0.1f;
+
+                    buildPos = new Vector3((int)_location.x, hit.point.y, (int)_location.z);
+                    PreviewPrefab.transform.position = buildPos;
+
+                }
+            }
+
+            if (PreviewPrefab != null && PreviewPrefab.GetComponentInChildren<Preview>().GetBuildable())
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Instantiate(InsPrefab, buildPos, Quaternion.identity);
+                    Destroy(PreviewPrefab);
+                    isActivatePreview = false;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Escape) && PreviewPrefab != null)
+            {
                 Destroy(PreviewPrefab);
                 isActivatePreview = false;
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.Escape) && PreviewPrefab != null)
+        public void DDD()
         {
-            Destroy(PreviewPrefab);
-            isActivatePreview = false;
+
         }
+
+
+        public void SlotClick(int _SlotNumber)
+        {
+            isActivatePreview = false;
+
+            PreviewPrefab = Instantiate(craft[_SlotNumber].previewCraft);
+            InsPrefab = craft[_SlotNumber].BuildCraft;
+            Debug.Log("프리뷰 : " + isActivatePreview);
+            isActivatePreview = true;
+            Debug.Log("프리뷰2 : " + isActivatePreview);
+        }
+
+
     }
-
-
-    public void SlotClick(int _SlotNumber)
-    {
-        PreviewPrefab = Instantiate(craft[_SlotNumber].previewCraft);
-        InsPrefab = craft[_SlotNumber].BuildCraft;
-        isActivatePreview = true;
-    }
-
-
-
 
 
 
--- Assets/Scripts/S_JSG/ui/Action.cs
+++ Assets/Scripts/S_JSG/ui/Action.cs
@@ -7,12 +7,19 @@ namespace UI.HUD
 {
     public class Action : MonoBehaviour
     {
+        public int slotnum;
+
         public void OnClick()
         {
              //ActionFrame.instance.StartSpawnTimer(name);
             InputManager.InputHandler.instance.UnitSpawn(name);
         }
+        public void BuildClick()
+        {
 
+
+            RTS.Player.BuildManager.instance.SlotClick(slotnum);
+        }
       
 
         
